3c78bb0ed18b4a76fbb09a12b36dc8cb044a9ad7
KIBANGU <Joekbg@users.noreply.github.com>
2021-02-18 14:11:39 +0100

    Delete chap_12.js

--- tps/7Wonders-Corrige/js/chap_12.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	$("#tous").click(function() {
-		var checked_status = this.checked;
-		$("input[name^=case]").each(function() {
-			this.checked = checked_status;
-		});
-	});
-});
